de85747cd71fbe9c703ffc6123c4b548a5aaeb14
Louis Fovet <louiscfovet@gmail.com>
2016-02-23 14:55:33 +0100

    [v0.1] Projet libGDX

---
+++ desktop/src/fr/telecom/durifgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.telecom.durifgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.telecom.durifgame.DurifGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DurifGame(), config);
+	}
+}
