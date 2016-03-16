c74cee85f08dc732f42b240e1b6f6b03cb4c2f12
MatthieuCotton <matthieu.cotton@orange.com>
2016-03-16 16:55:48 +0100

    Ajout d'un ecran d'accueil (desing a faire) et leger refactor

--- core/src/fr/telecom/durifgame/DurifGame.java
+++ core/src/fr/telecom/durifgame/DurifGame.java
@@ -1,23 +1,21 @@
 package fr.telecom.durifgame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 
-import fr.telecom.durifgame.player.Player;
-import fr.telecom.durifgame.scenes.Hud;
+import fr.telecom.durifgame.GameScreen;
 
-public class DurifGame extends ApplicationAdapter {
+
+
+public class DurifGame extends Game {
 	
 	private final static boolean DBG = true;
 	private final static String TAG = DurifGame.class.getSimpleName();
 	
-	private Map map ;
-    private Camera cam;
-    private KeyListener kListener;
-    private Player player;
-    private Music music;
-    private Hud hud;
+	protected final static int START_GAME = 1;
+
+    MainScreen mainScreen;
+    GameScreen gameScreen;
+    
     
 	//----------------------------------------------//
     //-create										//
@@ -25,12 +23,10 @@ public class DurifGame extends ApplicationAdapter {
     //----------------------------------------------//
 	@Override
 	public void create () {
-		cam = new Camera();
-        map = new Map("exemple.tmx");
-        player = new Player("soldier.png",cam);
-        music = new Music("lost-in-the-meadows.ogg");
-        kListener = new KeyListener(cam, map, player,music);
-        hud = new Hud();
+	
+		mainScreen = new MainScreen(this);
+		gameScreen = new GameScreen();
+		setScreen(mainScreen);
 	}
 
 	//-----------------------------------------------//
@@ -38,19 +34,7 @@ public class DurifGame extends ApplicationAdapter {
     //-----------------------------------------------//
 	@Override
 	public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        kListener.keyPressed();
-        if(player.checkPosition()){
-        	player.newGold();
-        }
-        cam.update();
-        map.setView(cam);
-        map.render();
-        player.displayPlayer();
-        
+		super.render();    
 	}
 	
 	//-----------------------------------------------//
@@ -58,15 +42,16 @@ public class DurifGame extends ApplicationAdapter {
     //-----------------------------------------------//
     @Override
     public void dispose() {
-    	if(map != null)
-    		map.dispose();
-    	if(music != null)
-    		music.dispose();
-    	
+    	super.dispose();
     }
     
-	public Music getMusic() {
-		return music;
-	}
-
+    protected void changeScreen(int key){
+    	switch(key){
+    	case START_GAME:
+    		setScreen(gameScreen);
+    		break;
+    	default:
+    			Log.logd(DBG, TAG, "changeScreen invalid key = "+key);
+    	}
+    }
 }
